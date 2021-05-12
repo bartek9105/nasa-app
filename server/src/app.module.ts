a4346c586af6bb6618a5071b3152af1a77a2069f
bartek9105 <bartek9105@gmail.com>
2021-05-12 11:51:46 +0200

    Config cron job for fetching and saving weather data every day

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,7 @@ import { PerservanceWeatherModule } from './perservance-weather/perservance-weat
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -9,6 +10,7 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_CONNECTION),
+    ScheduleModule.forRoot(),
     PerservanceWeatherModule,
   ],
   controllers: [AppController],
--- server/src/perservance-weather/perservance-weather.service.ts
+++ server/src/perservance-weather/perservance-weather.service.ts
@@ -1,15 +1,20 @@
-import { HttpService } from '@nestjs/common';
+import { HttpService, Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { PerservanceWeather } from './perservance-weather.interface';
 import { Model } from 'mongoose';
+import { Cron } from '@nestjs/schedule';
 
+@Injectable()
 export class PerservanceWeatherService {
+  private readonly logger = new Logger(PerservanceWeatherService.name);
+
   constructor(
     private httpService: HttpService,
     @InjectModel('PerservanceWeather')
     private readonly perservanceWeatherModel: Model<PerservanceWeather>,
   ) {}
 
+  @Cron('59 23 * * *')
   async fetchPerservanceWeather(): Promise<PerservanceWeather[]> {
     const apiUrl =
       'https://mars.nasa.gov/rss/api/?feed=weather&category=mars2020&feedtype=json';
@@ -19,6 +24,8 @@ export class PerservanceWeatherService {
       response.data.sols[response.data.sols.length - 1];
 
     this.savePerservanceWeather(lastSolWeatherData);
+
+    this.logger.log('Fetched and saved new data');
 
     return lastSolWeatherData;
   }
