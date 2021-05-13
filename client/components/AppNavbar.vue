1b69430738b6cd70d73dada3832b1fe2135e3748
bartek9105 <bartek9105@gmail.com>
2021-05-13 11:53:11 +0200

    Create navbar component

---
+++ client/components/AppNavbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="flex justify-between px-16 py-8 border-2 border-gray-100 shadow-sm"
+  >
+    <h1 class="text-purple-500 font-bold text-xl">Nasa App</h1>
+    <nav>
+      <ul class="flex">
+        <li class="mr-8 hover:text-purple-500 cursor-pointer">
+          Astronomy picture of the day
+        </li>
+        <li class="mr-8 hover:text-purple-500 cursor-pointer">Rover photos</li>
+        <li class="hover:text-purple-500 cursor-pointer">Mars weather</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppNavbar',
+}
+</script>
+
+<style></style>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <AppNavbar />
     <Nuxt />
   </div>
 </template>
+
+<script>
+import AppNavbar from '@/components/AppNavbar.vue'
+
+export default {
+  components: {
+    AppNavbar,
+  },
+}
+</script>
